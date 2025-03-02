9a3dc2e4db1442ea340b015beed53978ba0eed0b
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-03-02 16:16:30 +0300

    section .ts import composant class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Liste de pokemon !</h1>
 
     <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-pokemon-app';
+ 
+  ngOnInit(): void {
+      console.table(POKEMONS)
+      this.selectPokemon(POKEMONS[1])
+  }
+  selectPokemon(pokemonName:Pokemon){
+    console.log(`Vous avez clique sur le pokemon ${pokemonName.name}`)
+  }
 }
