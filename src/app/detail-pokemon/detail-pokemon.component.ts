b5c724e3511e1cc5c94df906084c72fd44639632
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-03-03 19:59:59 +0300

    detail pokemon

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+import { CommonModule } from '@angular/common';
+import { PokemonColorPipe } from '../pokemon-color.pipe';
 @Component({
   selector: 'app-detail-pokemon',
-  imports: [],
+  imports: [CommonModule,PokemonColorPipe],
   templateUrl: './detail-pokemon.component.html',
   styles: ``
 })
-export class DetailPokemonComponent {
-
+export class DetailPokemonComponent implements OnInit {
+  pokemonList:Pokemon[];
+  pokemon:Pokemon|undefined;
+  constructor(private router:ActivatedRoute){}
+  ngOnInit(): void {
+    this.pokemonList=POKEMONS;
+    const pokemonId:string|null= this.router.snapshot.paramMap.get('id');
+    this.pokemon=this.pokemonList.find(pokemon=>pokemon.id == (pokemonId!=undefined ? pokemonId :0))
+  }
 }
